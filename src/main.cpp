f7c6813fa9348dcd3de5f8589ac280662843e774
KevinBeltran23 <kevinbeltran2437@gmail.com>
2024-08-09 13:05:15 -0700

    pong Tutorial

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,177 @@
+#include <iostream>
 #include <raylib.h>
+
+using namespace std;
+
+Color Green = Color{38, 185, 154, 255};
+Color Dark_Green = Color{20, 160, 133, 255};
+Color Light_Green = Color{129, 204, 184, 255};
+Color Yellow = Color{243, 213, 91, 255};
+
+int player_score = 0;
+int cpu_score = 0;
+
+class Ball
+{
+public:
+    float x, y;
+    int speed_x, speed_y;
+    int radius; 
+
+    void Draw()
+    {
+        DrawCircle(x, y, radius, Yellow);
+    }
+
+    void Update()
+    {
+        x += speed_x;
+        y += speed_y;
+
+        if(y + radius >= GetScreenHeight() || y - radius <= 0)
+        {
+            speed_y *= -1;
+        }
+        if(x + radius >= GetScreenWidth())
+        {
+            cpu_score++;
+            ResetBall();
+        }
+        if(x - radius <= 0)
+        {
+            player_score++;
+            ResetBall();
+        }
+    }
+
+    void ResetBall()
+    {
+        x = GetScreenWidth()/2;
+        y = GetScreenHeight()/2;
+
+        int speed_choices[2] = {-1, 1};
+        speed_x *= speed_choices[GetRandomValue(0, 1)];
+        speed_y *= speed_choices[GetRandomValue(0, 1)];
+    }
+};
+
+class Paddle 
+{
+protected:
+    void LimitMovement()
+    {
+        if (y <= 0)
+        {
+            y = 0;
+        }
+        if(y + height >= GetScreenHeight())
+        {
+            y = GetScreenHeight() - height;
+        }
+    }
+public:
+    float x, y;
+    float width, height;
+    int speed;
+
+    void Draw() 
+    {
+        DrawRectangleRounded(Rectangle{x, y, width, height}, 0.8, 0, WHITE);
+    }
+
+    void Update()
+    {
+        if(IsKeyDown(KEY_UP))
+        {
+            y = y - speed;
+        }
+        if(IsKeyDown(KEY_DOWN))
+        {
+            y = y + speed;
+        }
+        LimitMovement();
+    }
+};
+
+class CpuPaddle: public Paddle
+{
+    public:
+
+    void Update(int ball_y)
+    {
+        if(y + height/2 > ball_y)
+        {
+            y = y - speed;
+        } else if(y + height/2 < ball_y){
+            y = y + speed;
+        } else {
+            y = y;
+        }
+        LimitMovement();
+    }
+};
+
+Ball ball;
+Paddle player;
+CpuPaddle cpu;
 
 int main() 
 {
-    int ballX = 400;
-    int ballY = 400;
-    Color green = {20, 160, 133, 255};
+    const int screen_width = 1280;
+    const int screen_height = 800;
 
-    InitWindow(800, 800, "My First Raylib Game");
+    InitWindow(screen_width, screen_height, "My Pong Game");
     SetTargetFPS(60);
+
+    ball.radius = 20;
+    ball.x = screen_width/2;
+    ball.y = screen_height/2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
+
+    player.width = 25;
+    player.height = 120;
+    player.x = screen_width - player.width - 10;
+    player.y = screen_height/2 - player.height/2;
+    player.speed = 6;
+
+    cpu.height = 120;
+    cpu.width = 25;
+    cpu.x = 10;
+    cpu.y = screen_height/2 - cpu.height/2;
+    cpu.speed = 6;
 
     // game loop
     while(WindowShouldClose() == false) 
     {
         // 1. Event Handling
-        if(IsKeyDown(KEY_RIGHT))
-        {
-            ballX += 3;
-        } else if(IsKeyDown(KEY_LEFT))
-        {
-            ballX -= 3;
-        } else if(IsKeyDown(KEY_UP))
-        {
-            ballY -= 3;
-        } else if(IsKeyDown(KEY_DOWN))
-        {
-            ballY += 3;
-        }
+        
         // 2. Updating Positions
+        ball.Update();
+        player.Update();
+        cpu.Update(ball.y);
 
+        // Checking for Collisions
+        if(CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player.x, player.y, player.width, player.height}))
+        {
+            ball.speed_x *= -1;
+        }
+        if(CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{cpu.x, cpu.y, cpu.width, cpu.height}))
+        {
+            ball.speed_x *= -1;
+        }
 
         // 3. Drawing
         BeginDrawing();
-        ClearBackground(green);
-        DrawCircle(ballX, ballY, 20, WHITE);
+        ClearBackground(Dark_Green);
+        DrawRectangle(screen_width/2, 0, screen_width/2, screen_height, Green);
+        DrawCircle(screen_width/2, screen_height/2, 150, Light_Green);
+        DrawLine(screen_width/2, 0, screen_width/2, screen_height, WHITE);
+        ball.Draw();
+        cpu.Draw();
+        player.Draw();
+        DrawText(TextFormat("%i", cpu_score), screen_width/4 - 20, 20, 80, WHITE);
+        DrawText(TextFormat("%i", player_score), 3*screen_width/4 - 20, 20, 80, WHITE);
 
 
         EndDrawing();
